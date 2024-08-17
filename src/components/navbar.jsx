e520dff32276a62ba54fecdd731f4d485ded63ab
Verosha Kriyanjala <veroshakriyanjala32@gmail.com>
2024-08-17 07:56:40 +0530

    add states to navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,7 +3,20 @@ import { Link } from "react-router-dom";
 import "./navbar.css";
 
 class NavBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeLink: "/home", // Set default active link
+    };
+  }
+
+  setActiveLink = (link) => {
+    this.setState({ activeLink: link });
+  };
+
   render() {
+    const { activeLink } = this.state;
+
     return (
       <nav className="navbar">
         <div className="brand">
@@ -11,19 +24,49 @@ class NavBar extends Component {
         </div>
         <ul className="nav-links">
           <li>
-            <Link to="/home">Home</Link>
+            <Link
+              to="/home"
+              className={activeLink === "/home" ? "active" : ""}
+              onClick={() => this.setActiveLink("/home")}
+            >
+              Home
+            </Link>
           </li>
           <li>
-            <Link to="/services">Services</Link>
+            <Link
+              to="/services"
+              className={activeLink === "/services" ? "active" : ""}
+              onClick={() => this.setActiveLink("/services")}
+            >
+              Services
+            </Link>
           </li>
           <li>
-            <Link to="/resume">Resume</Link>
+            <Link
+              to="/resume"
+              className={activeLink === "/resume" ? "active" : ""}
+              onClick={() => this.setActiveLink("/resume")}
+            >
+              Resume
+            </Link>
           </li>
           <li>
-            <Link to="/work">Work</Link>
+            <Link
+              to="/work"
+              className={activeLink === "/work" ? "active" : ""}
+              onClick={() => this.setActiveLink("/work")}
+            >
+              Work
+            </Link>
           </li>
           <li>
-            <Link to="/contactme" className="contact-button">
+            <Link
+              to="/contactme"
+              className={`contact-button ${
+                activeLink === "/contactme" ? "active" : ""
+              }`}
+              onClick={() => this.setActiveLink("/contactme")}
+            >
               <span>Contact Me</span>
             </Link>
           </li>
